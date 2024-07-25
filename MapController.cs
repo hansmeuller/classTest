2733eaf2f5609349f3ea66d57b75237e9b71a718
hansmeuller <118677299+hansmeuller@users.noreply.github.com>
2024-07-25 14:01:22 +0200

    create mapController

---
+++ MapController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klassenTest
+{
+    public class Poll
+    {
+        public int ProviderID { get; set; }
+        public long AmountNQT { get; set; }
+        public bool IsEntitled { get; set; }
+        public string HashValue { get; set; }
+        public long ActorID { get; set; }
+        public long TargetID { get; set; }
+        public string MainMethod { get; set; }
+        public string SubMethod { get; set; }
+        public long Parameter { get; set; }
+        public long Parameter2 { get; set; }
+        public DateTime Timeout { get; set; }
+        public int AgreedersCount { get; set; }
+        public int RejectersCount { get; set; }
+        public List<bool> Votes { get; set; } = new List<bool>();
+    }
+
+    public class MapController
+    {
+
+    }
+}
